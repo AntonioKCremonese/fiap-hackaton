d1fdff90033ce92ec8a0fa2ccfb80b7a4e91505c
Antonio K Cremonese <ak.cremonese@gmail.com>
2026-01-23 07:43:51 -0300

    first commit

---
+++ src/main/java/com/devs/fiap_hackaton/FiapHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.devs.fiap_hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapHackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/fiap_hackaton/FiapHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.fiap_hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapHackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
